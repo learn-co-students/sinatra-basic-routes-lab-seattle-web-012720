db498c053275bc9a30c11530ec829346cb62f1df
Agandaur <cjwagstaffe@gmail.com>
2020-02-18 11:32:11 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Chris"
+    end
+
+    get '/hometown' do
+        "My hometown is Solvang, CA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'All These Things that I have Done'"
+    end
 end
