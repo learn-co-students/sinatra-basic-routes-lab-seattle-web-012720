0f69e2c17db7d6cb57bc3dd1456745ab07e3eea0
javila35 <josepharthuravila@gmail.com>
2020-02-18 17:57:59 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Joe"
+    end
+
+    get '/hometown' do
+        "My hometown is San Diego, CA"
+    end
+
+    get '/favorite-song' do 
+        "My favorite song is currently 'Little Bit' by Erika de Casier"
+    end
+
+
 end
